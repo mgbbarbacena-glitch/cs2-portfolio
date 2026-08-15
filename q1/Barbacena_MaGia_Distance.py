497d34dd52c1efd36b97c6c98a14cb32628be41a
mgbbarbacena-glitch <mgbbarbacena@brc.pshs.edu.ph>
2026-08-15 22:57:36 +0800

    Update Barbacena_MaGia_Distance.py

--- q1/Barbacena_MaGia_Distance.py
+++ q1/Barbacena_MaGia_Distance.py
@@ -8,3 +8,7 @@ y2 = float(input("Enter y2:"))
 distance = math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
 
 print("The distance between the two points is:", round(distance, 2))
+
+# Reflection:
+# I learned that using a library is more practical because it makes the calculations easier.
+# The sqrt and pow functions helped me calculate the distance more easily.
