a92efcc2f6065ac1ea6ed0f3d150832115ea7dba
mgbbarbacena-glitch <mgbbarbacena@brc.pshs.edu.ph>
2026-09-05 16:24:18 +0800

    Update score_checker.py

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,1 +1,16 @@
+# Ask the user to enter a student score
+score = int(input("Enter student score: "))
 
+# Check if the score is outside the valid range
+if score < 0 or score > 100:
+  print("Invalid Score.")
+
+# Classify the score if it is valid
+elif score >= 90:
+  print("Outstanding")
+elif score >= 80:
+  print("Very Satisfactory")
+elif score >= 75:
+  print("Satisfactory")
+else:
+  print("Needs Improvement")
