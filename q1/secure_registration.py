b006c109a7d67d9b94115964b439ac8bd1e7a96c
mgbbarbacena-glitch <mgbbarbacena@brc.pshs.edu.ph>
2026-09-18 14:55:20 +0800

    Update secure_registration.py

--- q1/secure_registration.py
+++ q1/secure_registration.py
@@ -1,1 +1,44 @@
+name = input("Enter student name: ")
 
+if name == "":
+    print("REGISTRATION NOT ACCEPTED")
+    print("Student name is required.")
+
+else:
+    section = input("Enter section: ")
+
+    if section != "Dahlia":
+        print("REGISTRATION NOT ACCEPTED")
+        print("Invalid section.")
+
+    else:
+        club = input("Enter club choice: ")
+
+        if club not in ["Robotics", "Science", "Mathematics", "Programming"]:
+            print("REGISTRATION NOT ACCEPTED")
+            print("Please choose a valid club.")
+
+        else:
+            email = input("Enter school email: ")
+
+            if "@" not in email or "." not in email:
+                print("REGISTRATION NOT ACCEPTED")
+                print("Invalid school email.")
+
+            else:
+                attendance = input("Enter attendance status: ")
+
+                if attendance not in ["Present", "Absent", "Late"]:
+                    print("REGISTRATION NOT ACCEPTED")
+                    print("Invalid attendance status.")
+
+                else:
+                    print("--------------------------------")
+                    print("REGISTRATION ACCEPTED")
+                    print("--------------------------------")
+                    print("Student:", name)
+                    print("Section:", section)
+                    print("Club:", club)
+                    print("Email:", email)
+                    print("Attendance:", attendance)
+                  
